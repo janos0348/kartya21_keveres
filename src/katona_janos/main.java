e27feef084a012f742515e53cefced1482a93ad4
janos0348 <katonajanos0128@gmail.com>
2022-09-11 22:31:08 +0200

    gitignore file

---
+++ src/katona_janos/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package katona_janos;
+public class main {
+    static String[] szinek = {"zőld","piros","makk","tök"};
+    static String[] figurak = {"A","K","Q","J","10","9"};
+    static String[] pakli = new String[21];
+    public static void main(String[] args) {
+    }
+    
+}
