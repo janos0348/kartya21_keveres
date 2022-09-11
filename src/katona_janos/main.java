99cec7bbda0be6a00f0f965cd321485785dfa00e
janos0348 <katonajanos0128@gmail.com>
2022-09-11 22:34:40 +0200

    feltolt metódus

--- src/katona_janos/main.java
+++ src/katona_janos/main.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package katona_janos;
+
 public class main {
-    static String[] szinek = {"zőld","piros","makk","tök"};
-    static String[] figurak = {"A","K","Q","J","10","9"};
+
+    static String[] szinek = {"zőld", "piros", "makk", "tök"};
+    static String[] figurak = {"A", "K", "Q", "J", "10", "9"};
     static String[] pakli = new String[21];
+    static int random;
     public static void main(String[] args) {
+        feltolt();
     }
-    
-}
+
+    private static void feltolt() {
+        for (String kartya : figurak) {
+            for (int i = 0; i < 4; i++) {
+                do {
+                    random = (int) (Math.random() * (21 - 1) + 1);
+                } while (!(pakli[random] == null));
+
+                pakli[random] = "+";
+                System.out.println(kartya);
+            }
+
+        }
